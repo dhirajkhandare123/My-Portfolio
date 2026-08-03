1e5bb6d515f714fed7dab2acce48cd3f96d22822
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-03 22:07:50 +0530

    Update App.jsx

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar'
+import Home from './components/Home'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='flex'>
+    <div className='flex bg-gradient-to-tl from-pink-500 to-indigo-600'>
       <Sidebar/>
       <main className='flex-1'>
-        
+        <Home/>
       </main>
     </div>
   )
