2986bfe38952419a0c5f6e98343fc084fb598c9b
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-03 14:54:50 +0530

    created react project

---
+++ protfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Sidebar from './components/Sidebar'
+
+const App = () => {
+  return (
+    <div className='flex'>
+      <Sidebar/>
+      <main className='flex-1'>
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
