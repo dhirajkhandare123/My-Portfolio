58ecc81fb07e245c571e9d244f9a92b649ec0977
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-05 21:46:52 +0530

    Create CourseWork.jsx

---
+++ protfolio/src/components/CourseWork.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+
+const CourseWork = () => {
+  return (
+    <section className='h-full'>
+        <div className='justify-center items-center pt-10 h-screen'>
+            <h1 className='text-4xl font-bold text-center mb-10 text-white underline'>Course Work</h1>
+        </div>
+
+        <Footer/>
+    </section>
+  )
+}
+
+export default CourseWork
